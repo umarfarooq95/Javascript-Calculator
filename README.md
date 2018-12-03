3af34bc38d4c0c3b38c1c924f5e864f862853c4a
umarfarooq95 <mohamedumar931@gmail.com>
2018-12-03 19:37:50 +0530

    delete style.css file

--- README.md
+++ README.md
@@ -1,8 +1,28 @@
 # Javascript-Calculator
-Simple Javascript Calculator
+> This is a Simple Javascript Calculator
 <p align="center">
   <img src="./src/images/calc.png" width="350" title="Calculator">
 </p>
+
+## Note
+#### Add index.js to your html file
+
+## Example
+```html
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Calculator</title>
+    <script src="../node_modules/src/index.js"></script>
+</head>
+<body>
+</body>
+</html>
+```
+
 
 Step 1: add some class to the div.
 
--- src/index.js
+++ src/index.js
@@ -2,8 +2,59 @@
 
     $.fn.javascriptCalcInit = init;
 
+    var root = {
+        margin: 'auto',
+        width: '480px',
+        height: '355px',
+        border: '2px solid black',
+        marginTop: '50px',
+        background: '#e8e8e8',
+        borderRadius: '5px',
+        "box-sizing": 'border-box'
+    };
+    var value = {
+        width: '90%',
+        margin: '19px',
+        height: '35px',
+        padding: '10px'
+    };
+
+    var bodyWrapperClassObj = {
+        display: 'flex'
+    };
+
+    var numbersWrapperClassObj = {
+        width: '345px',
+        display: 'flex',
+        flexWrap: 'wrap',
+        flexDirection: 'row-reverse'
+    };
+
+    var operatorsWrapperClassObj = {
+        display: 'flex',
+        flexDirection: 'column'
+    };
+
+    var numbersClassObj = {
+        width: '75px',
+        height: '40px',
+        display: 'inline-block',
+        margin: '10px 19px 19px 19px',
+        border: '2px solid lightblue',
+        padding: '10px',
+        background: 'white',
+        cursor: 'pointer',
+        borderRadius: '5px'
+    }
+    var styleTag = '<style>*{box-sizing: border-box;body{margin: 0;}'+
+    'input[type=number]::-webkit-inner-spin-button,'+
+    'input[type=number]::-webkit-outer-spin-button {'+
+        '-webkit-appearance: none;'+
+        'margin: 0;'+
+    '}<style>';
+
     function init() {
-        $(this).addClass('root')
+        $(this).css(root).addClass('root').append(styleTag);
         buildBasicStructure();
         buildNumbers();
         buildClearAndEqualDivs()
@@ -11,10 +62,10 @@
     }
 
     function buildBasicStructure(){
-        var input = $('<input/>').addClass('value').attr('readonly', true);
-        var bodyWrapper = $('<div></div>').addClass('body-wrapper');
-        var numbersWrapper = $('<div></div>').addClass('numbers-wrapper')
-        var operatorsWrapper = $('<div></div>').addClass('operators-wrapper')
+        var input = $('<input/>').css(value).addClass('value').attr('readonly', true);
+        var bodyWrapper = $('<div></div>').css(bodyWrapperClassObj).addClass('body-wrapper');
+        var numbersWrapper = $('<div></div>').css(numbersWrapperClassObj).addClass('numbers-wrapper')
+        var operatorsWrapper = $('<div></div>').css(operatorsWrapperClassObj).addClass('operators-wrapper')
         $(bodyWrapper).append(numbersWrapper);
         $(bodyWrapper).append(operatorsWrapper);
         $(".root").append(input);
@@ -24,8 +75,8 @@
     function buildNumbers() {
         var numberWrapper = $('.numbers-wrapper')
         for (var i = 0; i < 10; i++) {
-            var numbersDiv = $('<div></div>').addClass('numbers').attr('id', 'numbersDiv' + i);
-            var textDiv = $('<div></div>').addClass('text-center')
+            var numbersDiv = $('<div></div>').css(numbersClassObj).addClass('numbers').attr('id', 'numbersDiv' + i);
+            var textDiv = $('<div></div>').css({textAlign: 'center'})
             textDiv[0].innerText = i
             $(numbersDiv).append(textDiv);
             $(numberWrapper).append(numbersDiv)
@@ -38,8 +89,8 @@
         var arr = ['c', '='];
         var numberWrapper = $('.numbers-wrapper')
         for (var j = 0; j < arr.length; j++) {
-            var numbersDiv = $('<div></div>').addClass('numbers').attr('id', 'clearOrEqualDivs' + j);
-            var textDiv = $('<div></div>').addClass('text-center')
+            var numbersDiv = $('<div></div>').css(numbersClassObj).addClass('numbers').attr('id', 'clearOrEqualDivs' + j);
+            var textDiv = $('<div></div>').css({textAlign: 'center'})
             textDiv[0].innerText = arr[j]
             $(numbersDiv).append(textDiv);
             $(numberWrapper).append(numbersDiv)
@@ -72,8 +123,8 @@
         var operators = ['+', '-', '*', '/'];
         var operatorWrapper = $('.operators-wrapper')
         for (var j = 0; j < operators.length; j++) {
-            var numbersDiv = $('<div></div>').addClass('numbers').attr('id', 'operatorsDivs' + j);
-            var textDiv = $('<div></div>').addClass('text-center')
+            var numbersDiv = $('<div></div>').css(numbersClassObj).addClass('numbers').attr('id', 'operatorsDivs' + j);
+            var textDiv = $('<div></div>').css({textAlign: 'center'})
             textDiv[0].innerText = operators[j]
             $(numbersDiv).append(textDiv);
             $(operatorWrapper).append(numbersDiv)
@@ -91,3 +142,7 @@
     }
 
 }(jQuery));
+
+$(document).ready(function(){
+    $('.wrapper').javascriptCalcInit()
+})
